68c2a5d25168fcb34dff212c03f6584563b16c86
Colin Zarzycki <zarzycki@ucar.edu>
2017-11-09 15:32:12 -0700

    snowstorm code modifs, JRA preprocess code

--- projects/snowstorms/process-JRA55-for-RSI/PRECT-SNOW/driver.sh
+++ projects/snowstorms/process-JRA55-for-RSI/PRECT-SNOW/driver.sh
@@ -17,7 +17,7 @@ JRABASEDIR=/glade/p/rda/data/ds628.0/
 SYMDIR=/glade/u/home/zarzycki/scratch/JRAsym/
 OUTBASE=/glade/scratch/zarzycki/h1files/JRA/
 
-for YYYY in `seq 1960 2012`; do
+for YYYY in `seq 1969 2016`; do
 
   if [ ! -f ${OUTBASE}/${YYYY}/JRA.h1.${YYYY}.PRECT.nc ]; then
 
@@ -25,16 +25,21 @@ for YYYY in `seq 1960 2012`; do
 
     ### Symlink JRA files
     mkdir -p ${SYMDIR}/${YYYY}
-    rm ${SYMDIR}/${YYYY}/*.grb2
+#    rm ${SYMDIR}/${YYYY}/*.grb2
 
     declare -a prect_arr=("srweq" "tprat")
     for i in "${prect_arr[@]}"
     do
-      FILES=${JRABASEDIR}/fcst_phy2m/${YYYY}/*_${i}.reg_tl319.*
+      if [ "${i}" == "prmsl" ]; then
+        FILES=${JRABASEDIR}/fcst_surf/${YYYY}/*_${i}.reg_tl319.*
+      else
+        FILES=${JRABASEDIR}/fcst_phy2m/${YYYY}/*_${i}.reg_tl319.*
+      fi
       for f in $FILES
       do
         echo "Processing $f file..."
         a=$(basename $f)
+        rm ${SYMDIR}/${YYYY}/${a}.grb2
         ln -s ${f} ${SYMDIR}/${YYYY}/${a}.grb2
       done
     done
