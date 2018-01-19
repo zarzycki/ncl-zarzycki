85739f463a37db90279764a2b12a37e26ee4619a
Colin Zarzycki <zarzycki@ucar.edu>
2018-01-19 14:13:29 -0700

    add test dir, modifications to GFS/CAM ACC skill code

--- projects/tcforecast/CAM_GFS_CFSR_skill/driver.sh
+++ projects/tcforecast/CAM_GFS_CFSR_skill/driver.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /glade/scratch/zarzycki/forecast_conus_30_x8_CAM5/run/ 
-for i in $( ls -d 2017* ); do
+for i in $( ls -d 201* ); do
   echo $i
   cd /glade/u/home/zarzycki/ncl/projects/tcforecast/CAM_GFS_CFSR_skill/
   ncl -n analyzeData.ncl YYYYMMDDHH=$i
