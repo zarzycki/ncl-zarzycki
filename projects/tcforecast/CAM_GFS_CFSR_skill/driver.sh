90f36cc6bde3e7b4b56b700946abf4ea9258c534
Colin Zarzycki <zarzycki@ucar.edu>
2018-01-23 16:36:20 -0700

    updates to GFS/CAM ACC skill code for AMWG

--- projects/tcforecast/CAM_GFS_CFSR_skill/driver.sh
+++ projects/tcforecast/CAM_GFS_CFSR_skill/driver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /glade/scratch/zarzycki/forecast_conus_30_x8_CAM5/run/ 
+cd /glade/scratch/zarzycki/hindcast_conus_30_x8_CAM4_L26/run/ 
 for i in $( ls -d 201* ); do
   echo $i
   cd /glade/u/home/zarzycki/ncl/projects/tcforecast/CAM_GFS_CFSR_skill/
