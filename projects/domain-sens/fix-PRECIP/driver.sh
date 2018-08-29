b91414cb3bbf607ed66421f3ed2b4188772fc71b
Colin Zarzycki <zarzycki@ucar.edu>
2018-08-28 20:37:53 -0700

    cori add 6hrly ptype driver modifs for hyperion runs

--- projects/domain-sens/fix-PRECIP/driver.sh
+++ projects/domain-sens/fix-PRECIP/driver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CONFIG=CHEY.VR28.NATL.REF.CAM5.4CLM5.0.dtime900.002
+CONFIG=CORI.VR28.NATL.EXT.CAM5.4CLM5.0.dtime900.003
 FILES=`find ~/scratch/hyperion/${CONFIG}/atm/hist/ -name '*h4.*' -print | sort`
 for f in $FILES
 do
