cbdde911aa341df12d1e8a7da633c246abb69770
Colin Zarzycki <zarzycki@ucar.edu>
2018-09-20 16:27:54 -0700

    added code to handle ET in hyp runs

--- projects/domain-sens/process-ET/drive-process-vort.sh
+++ projects/domain-sens/process-ET/drive-process-vort.sh
@@ -11,13 +11,13 @@
 #>#PBS -l select=1:ncpus=36:mem=109GB
 ################################################################
 #SBATCH -N 1                #Use 2 nodes
-#SBATCH -t 06:00:00         #Set 30 minute time limit
+#SBATCH -t 07:59:00         #Set 30 minute time limit
 #SBATCH -q regular          #Use the regular QOS
 #SBATCH -L SCRATCH          #Job requires $SCRATCH file system
 #SBATCH -C knl,quad,cache   #Use KNL nodes in quad cache format (default, recommended)
 ################################################################
 
-GRID=WAT
+GRID=EXT
 CONFIG=dtime900.003
 
 starttime=$(date -u +"%s")
@@ -25,15 +25,16 @@ starttime=$(date -u +"%s")
 module load parallel
 module load ncl 
 
-NUMCORES=8
+NUMCORES=12
 TIMESTAMP=`date +%s%N`
 COMMANDFILE=commands.${TIMESTAMP}.txt
 
-nclweightfile="/global/homes/c/czarzyck/scratch/maps/hyperion/map_ne0np4natlantic${GRID,,}.ne30x4_to_2.0x2.0_GLOB.nc"
-for f in /global/homes/c/czarzyck/scratch/hyperion/CORI.VR28.NATL.${GRID}.CAM5.4CLM5.0.${CONFIG}/atm/hist/*.h2.*.nc
+nclweightfile="/global/homes/c/czarzyck/scratch/maps/hyperion/map_ne0np4natlantic${GRID,,}.ne30x4_to_0.25x0.25_GLOB.nc"
+FILES=`find /global/homes/c/czarzyck/scratch/hyperion/CORI.VR28.NATL.${GRID}.CAM5.4CLM5.0.${CONFIG}/atm/hist/ -name "*h2.???4*"`
+for f in ${FILES}
 do
   #ncl process-vort.ncl 'infile="'${f}'"' 'wgt_file="'${nclweightfile}'"'
-  NCLCOMMAND="ncl process-vort.ncl 'infile=\"'${f}'\"' 'wgt_file=\"'${nclweightfile}'\"'     "
+  NCLCOMMAND="ncl create-files.ncl 'f2name=\"'${f}'\"' 'wgt_file=\"'${nclweightfile}'\"'     "
   echo ${NCLCOMMAND} >> ${COMMANDFILE}
 done
 
@@ -47,3 +48,5 @@ tottime=$(($endtime-$starttime))
 rm ${COMMANDFILE}
 
 printf "${tottime}\n" >> timing.txt
+
+
