073688c086fafe70b898173844f6ceb4d164e84e
Colin Zarzycki <zarzycki@ucar.edu>
2023-04-14 12:43:49 -0700

    rewrite freezing code

---
+++ projects/reanalysis_process/TGW/drive-freezing.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+
+################################################################
+#SBATCH -N 1                #Number of nodes
+#SBATCH -t 40:00:00         #Time limit
+#SBATCH -q premium          #Use the regular QOS
+#SBATCH -L SCRATCH          #Job requires $SCRATCH file system
+#SBATCH -C knl,quad,cache
+################################################################
+
+NUMCORES=8
+TIMESTAMP=`date +%s%N`
+COMMANDFILE=commands.${TIMESTAMP}.txt
+
+FILES=`find /global/cfs/cdirs/m2702/gsharing/tgw-wrf-conus/historical_1980_2019/three_hourly/ -name "*nc" | sort -n`
+for f in $FILES; do
+  LINECOMMAND="ncl create-freezing-2.ncl 'f=\"$f\"'   "
+  echo ${LINECOMMAND} >> ${COMMANDFILE}
+done
+
+parallel --jobs ${NUMCORES} --workdir $PWD < ${COMMANDFILE}
